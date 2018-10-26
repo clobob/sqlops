95d41ad666a26bcfa74b08adfe956af278334179
Dou Liyou <douliyou@live.cn>
2018-10-26 11:34:13 +0800

    修复sqladvisor执行后没有返回的问题.

--- php_scripts/sqladvisor_config.php
+++ php_scripts/sqladvisor_config.php
@@ -1,13 +1,13 @@
 <?php
-	
+  $remote_host="172.18.62.143";	
   $remote_user="root";   
-  $remote_password="gta@2015";   
-  $connection = ssh2_connect('192.168.199.199',22);  
-  $script='/usr/bin/sqladvisor -h '.$ip.' -u '.$user.' -p '.$pwd.' -P '.$port.' -d '.$db.' -q "'.$multi_sql[$x].'"'; 
+  $remote_password="1233211234567";   
+  $connection = ssh2_connect($remote_host,22);  
+  $script='/usr/bin/sqladvisor -h '.$ip.' -u '.$user.' -p '.$pwd.' -P '.$port.' -d '.$db.' -q "'.$multi_sql[$x].'"'.' -v 1'; 
   ssh2_auth_password($connection,$remote_user,$remote_password);
   $stream = ssh2_exec($connection,$script);
   $errorStream = ssh2_fetch_stream($stream, SSH2_STREAM_STDERR);
   stream_set_blocking($errorStream, true);
   $message=stream_get_contents($errorStream);
-
+  $message=str_replace(array("\r\n","\r","\n"),"<br/>",$message);
 ?>
