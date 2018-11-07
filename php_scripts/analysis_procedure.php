301634a7a19cf539568fb6ea92a8b3a4ab254a7a
clobob <douliyou@live.cn>
2018-11-07 11:08:02 +0800

    更改分析过程中语句判断规则-replace/update ~1

--- php_scripts/analysis_procedure.php
+++ php_scripts/analysis_procedure.php
@@ -68,7 +68,7 @@ foreach ($procedureArray as $procedure){
 			continue;
 		}
 		#if((stripos ($sql,'update')>=0)||(stripos($sql,'replace')>=0)||preg_match('/(\/\*[^~]*\*\/)[\s]*[(update)|(replace)]/i',$sql)){
-		if(preg_match('/(\/\*[^~]*\*\/)[\s]*[(update)|(replace)]/i',$sql)){
+		if(preg_match('/(\/\*[^~]*\*\/)?[\s]*[(update)|(replace)]/i',$sql)){
 			$sqlRulesObj->updateRules($sql);
 			$sql_number++;
 			continue;
